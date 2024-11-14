051742ebb9e39963a4cb3d8320b29fb56842fcf8
Ellis Caird <ellisc15060@gmail.com>
2024-11-14 21:39:52 +0000

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ import markdownIt from "markdown-it";
 import markdownItAnchor from "markdown-it-anchor";
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 import clean from "eleventy-plugin-clean";
+import { statSync } from "fs";
+import { execSync } from "child_process";
 
 export default (config) => {
     
@@ -11,12 +13,25 @@ export default (config) => {
     config.addPassthroughCopy("src/style");
     config.addPassthroughCopy("src/sketches");
     config.addPassthroughCopy("src/mathjax-init.js");
+    config.addPassthroughCopy("src/assets");
 
     const markdownLibrary = markdownIt({ html: true }).use(markdownItAnchor, {
         permalink: false,
         slugify: s => s.toLowerCase().replace(/[^\w]+/g, '-')
     });
 
+    config.addFilter("lastModified", (filePath) => {
+        const stats = statSync(filePath);
+        return stats.mtime;
+    });
+
+    config.addFilter("gitLastModified", (filePath) => {
+        try {
+            return execSync(`git log -1 --format=%cI ${filePath}`).toString().trim();
+        } catch (error) {
+            return null; // Handle files that may not be tracked by Git
+        }
+    });
 
 
     config.setLibrary("md", markdownLibrary);
--- src/posts/japan/japanTest2.md
+++ src/posts/japan/japanTest2.md
@@ -1,6 +1,10 @@
 ---
 title : Japan Test 2
+author: Ellis Caird
 tags:
     - posts
     - japan
+date: 2024-11-14
 ---
+
+
